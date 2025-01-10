019429a86cd3b0104a2c796ef58eddcd6093abfb
Leo <dev@tjlabscorp.com>
2025-01-10 12:27:13 +0900

    feat: model for UVD
    make UVDModel class

--- TJLabsCommon/UVD/UVDModel.swift
+++ TJLabsCommon/UVD/UVDModel.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-public struct TJLabsSensorData {
+// MARK: - Sensors
+public struct SensorData {
     public var time: Int
     public var acc: [Double]
     public var userAcc: [Double]
@@ -10,7 +11,6 @@ public struct TJLabsSensorData {
     public var att: [Double]
     public var quaternion: [Double]
     public var rotationMatrix: [[Double]]
-    
     public var gameVector: [Float]
     public var rotVector: [Float]
     public var pressure: [Double]
@@ -35,77 +35,71 @@ public struct TJLabsSensorData {
     }
 }
 
-public struct TJLabsCollectData {
-    public var time: Int
-    public var acc: [Double]
-    public var userAcc: [Double]
-    public var gyro: [Double]
-    public var mag: [Double]
-    public var grav: [Double]
-    public var att: [Double]
-    public var quaternion: [Double]
-    public var rotationMatrix: [[Double]]
+// MARK: - UVD
+struct UnitDistance: Equatable {
+    public var index: Int = 0
+    public var length: Double = 0
+    public var velocity: Double = 0
+    public var isIndexChanged: Bool = false
+}
+
+public struct UserVelocity: Encodable {
+    let user_id: String
+    let mobile_time: Int
+    let index: Int
+    let length: Double
+    let heading: Double
+    let looking: Bool
     
-    public var gameVector: [Float]
-    public var rotVector: [Float]
-    public var pressure: [Double]
-    public var trueHeading: Double
-    public var magneticHeading: Double
-    
-    public var index: Int
-    public var length: Double
-    public var heading: Double
-    public var lookingFlag: Bool
-    public var isIndexChanged: Bool
-    
-    public var bleRaw: [String: Double]
-    public var bleAvg: [String: Double]
-    
-    public init() {
-        self.time = 0
-        self.acc = [Double](repeating: 0, count: 3)
-        self.userAcc = [Double](repeating: 0, count: 3)
-        self.gyro = [Double](repeating: 0, count: 3)
-        self.mag = [Double](repeating: 0, count: 3)
-        self.grav = [Double](repeating: 0, count: 3)
-        self.att = [Double](repeating: 0, count: 3)
-        self.quaternion = [Double](repeating: 0, count: 4)
-        self.rotationMatrix = [[Double]](repeating: [Double](repeating: 0, count: 3), count: 3)
-        self.gameVector = [Float](repeating: 0, count: 4)
-        self.rotVector = [Float](repeating: 0, count: 5)
-        self.pressure = [0]
-        self.trueHeading = 0
-        self.magneticHeading = 0
-        self.index = 0
-        self.length = 0
-        self.heading = 0
-        self.lookingFlag = false
-        self.isIndexChanged = false
-        self.bleRaw = [String: Double]()
-        self.bleAvg = [String: Double]()
+    public init(user_id: String, mobile_time: Int, index: Int, length: Double, heading: Double, looking: Bool) {
+        self.user_id = user_id
+        self.mobile_time = mobile_time
+        self.index = index
+        self.length = length
+        self.heading = heading
+        self.looking = looking
     }
+}
+
+public enum UserMode{
+    case MODE_PEDESTRIAN, MODE_VEHICLE, MODE_AUTO
 }
 
 struct Attitude: Equatable {
-    public var Roll: Double = 0
-    public var Pitch: Double = 0
-    public var Yaw: Double = 0
+    var roll: Double = 0
+    var pitch: Double = 0
+    var yaw: Double = 0
+    
+    func isNaN() -> Bool {
+        return roll.isNaN || pitch.isNaN || yaw.isNaN
+    }
+    
+    func toDegree() -> Attitude {
+        return Attitude(roll: TJLabsUtilFunctions.shared.radian2degree(radian: roll),
+                        pitch: TJLabsUtilFunctions.shared.radian2degree(radian: pitch),
+                        yaw: TJLabsUtilFunctions.shared.radian2degree(radian: yaw))
+    }
 }
 
 struct SensorAxisValue: Equatable {
     public var x: Double = 0
     public var y: Double = 0
     public var z: Double = 0
-    
     public var norm: Double = 0
 }
 
-enum Type {
+enum SensorPatternType {
     case NONE, PEAK, VALLEY
 }
 
+struct TimestampDouble: Equatable {
+    public var timestamp: Double = 0
+    public var valuestamp: Double = 0
+}
+
+
 struct PeakValleyStruct {
-    public var type: Type = Type.NONE
+    public var type: SensorPatternType = SensorPatternType.NONE
     public var timestamp: Double = 0
     public var pvValue: Double = 0.0
 }
@@ -117,20 +111,6 @@ struct StepResult: Equatable {
     public var stepLength: Double = 0
     public var isLooking: Bool = true
 }
-
-struct UnitDistance: Equatable {
-    public var index: Int = 0
-    public var length: Double = 0
-    public var velocity: Double = 0
-    public var isIndexChanged: Bool = false
-}
-
-
-struct TimestampDouble: Equatable {
-    public var timestamp: Double = 0
-    public var valuestamp: Double = 0
-}
-
 
 struct StepLengthWithTimestamp: Equatable {
     public var timestamp: Double = 0
@@ -168,20 +148,7 @@ public struct UnitDRInfo {
 }
 
 
-public struct UserVelocity: Encodable {
-    let user_id: String
-    let mobile_time: Int
-    let index: Int
-    let length: Double
-    let heading: Double
-    let looking: Bool
-    
-    public init(user_id: String, mobile_time: Int, index: Int, length: Double, heading: Double, looking: Bool) {
-        self.user_id = user_id
-        self.mobile_time = mobile_time
-        self.index = index
-        self.length = length
-        self.heading = heading
-        self.looking = looking
-    }
+// MARK: - Protocol
+public protocol UVDGeneratorDelegate: AnyObject {
+    func onUVDResult(_ generator: UVDGenerator, userVelocity: UserVelocity)
 }
