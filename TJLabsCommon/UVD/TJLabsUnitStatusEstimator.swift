c8e5a8f905a46544bf9b97117e2375ae419b29c1
Leo <dev@tjlabscorp.com>
2025-01-10 14:35:25 +0900

    feat: TJLabsUnitStatusEstimator
    class to estimate status

--- TJLabsCommon/UVD/TJLabsUnitStatusEstimator.swift
+++ TJLabsCommon/UVD/TJLabsUnitStatusEstimator.swift
@@ -1,8 +1,44 @@
-//
-//  TJLabsUnitStatusEstimator.swift
-//  TJLabsCommon
-//
-//  Created by 신동현 on 1/10/25.
-//
 
 import Foundation
+
+class TJLabsUnitStatusEstimator: NSObject {
+    override init() { }
+    
+    var lookingFlagStepQueue = LinkedList<Bool>()
+    let lookingFlagCheckIndexSize: Int = 3
+    
+    func estimateStatus(Attitude: Attitude, isIndexChanged: Bool) -> Bool {
+        if (isIndexChanged) {
+            let isLookingAttitude = (abs(Attitude.roll) < TJLabsUtilFunctions.shared.degree2radian(degree: 25) && Attitude.pitch > TJLabsUtilFunctions.shared.degree2radian(degree: -20) && Attitude.pitch < TJLabsUtilFunctions.shared.degree2radian(degree: 80))
+            updateIsLookingAttitudeQueue(lookingFlag: isLookingAttitude)
+            return checkLookingAttitude(lookingFlagStepQueue: lookingFlagStepQueue)
+        } else {
+            return false
+        }
+    }
+    
+    func checkLookingAttitude(lookingFlagStepQueue: LinkedList<Bool>) -> Bool {
+        if (lookingFlagStepQueue.count <= 2) {
+            return true
+        } else {
+            var bufferSum = 0
+            for i in 0..<lookingFlagStepQueue.count {
+                let value = lookingFlagStepQueue.node(at: i)!.value
+                if (value) { bufferSum += 1 }
+            }
+            
+            if (bufferSum >= 2) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    
+    func updateIsLookingAttitudeQueue(lookingFlag: Bool) {
+        if (lookingFlagStepQueue.count >= lookingFlagCheckIndexSize) {
+            _ = lookingFlagStepQueue.pop()
+        }
+        lookingFlagStepQueue.append(lookingFlag)
+    }
+}
