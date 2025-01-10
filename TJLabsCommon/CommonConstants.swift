e6232fc73d5c0b322ee34ae7dbaf0d3885279e64
Leo <dev@tjlabscorp.com>
2025-01-10 17:45:00 +0900

    refactor: CommonConstants
    remove useless variables

--- TJLabsCommon/CommonConstants.swift
+++ TJLabsCommon/CommonConstants.swift
@@ -1,8 +1,4 @@
 public class CommonConstants {
     public static let OPERATING_SYSTEM: String = "iOS"
-    public static let MODE_DR = "dr"
-    public static let MODE_PDR = "pdr"
-    public static let MODE_AUTO = "auto"
-
     public static let COMMON_HEADER: String = "(TJLabsCommon)"
 }
