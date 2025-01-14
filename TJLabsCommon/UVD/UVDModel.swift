7d2b3127c7ac2082b4110648d4fe79a9be375018
Leo <dev@tjlabscorp.com>
2025-01-14 17:03:35 +0900

    feat: UVDModel
    update delegate to send pressure

--- TJLabsCommon/UVD/UVDModel.swift
+++ TJLabsCommon/UVD/UVDModel.swift
@@ -151,6 +151,7 @@ public struct UnitDRInfo {
 // MARK: - Protocol
 public protocol UVDGeneratorDelegate: AnyObject {
     func onUvdResult(_ generator: UVDGenerator, userVelocity: UserVelocity)
+    func onPressureResult(_ generator: UVDGenerator, hPa: Double)
     func onVelocityResult(_ generator: UVDGenerator, kmPh: Double)
     func onUvdPauseMillis(_ generator: UVDGenerator, time: Double)
     func onUvdError(_ generator: UVDGenerator, error: String)
